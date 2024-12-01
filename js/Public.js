f2aa51254fe23af63811b4f7550ab5da41e3be6d
Abdallah Safan <abdallahSafan05@gmail.com>
2024-12-01 20:31:25 +0200

    dir3

--- js/Public.js
+++ js/Public.js
@@ -1,8 +1,6 @@
-document.addEventListener('DOMContentLoaded',toggle);
-
-function toggle(){
+document.addEventListener('DOMContentLoaded',function(){
     if(document.cookie.indexOf('loggedIn=true') !== -1) {
         document.getElementById('login').innerHTML = 'Logout';
         document.getElementById('login').href = './Logout.html';
     }
-}
+});
